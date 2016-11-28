6d5c152f639fc5f5b8cb8e909e5c961d8b80aa4f
Michelle Epstein <epstein.michelle@gmail.com>
2016-11-28 19:05:43 +0000

    Refactored release_bike spec to one liner

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,8 +2,6 @@ require 'docking_station'
 
 describe DockingStation do
 
-it "Can release a bike from a docking station" do
-  it { is_expected.to respond_to :release_bike }
-end
+it { is_expected.to respond_to :release_bike }
 
 end
